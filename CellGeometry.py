20cac156cfb1e42bbca1904af37ef9b6964227f0
nlhw <microGen@users.noreply.github.com>
2019-07-13 23:37:42 +0200

    Made yet some changes to geometry classes: edges and faces return dimensions

--- CellGeometry.py
+++ CellGeometry.py
@@ -1,17 +1,18 @@
 from operator import add, sub
+from math import sqrt
 
 class Geometry:
     """Base geometry, single vertex"""
 
-    def __init__(self, location, geoID):
+    def __init__(self, location, geometry_ID):
         self.__location = location
-        self.__geoID = geoID
+        self.__geometry_ID = geometry_ID
 
     def getLocation(self):
         return self.__location
 
     def getID(self):
-        return self.__geoID
+        return self.__geometry_ID
 
 ########################################################################################################################
 ########################################################################################################################
@@ -20,9 +21,9 @@ class Geometry:
 class CompGeom(Geometry):
     """Complex, compound geometry, objects with multiple vertices"""
 
-    def __init__(self, verts, location, geoID):
+    def __init__(self, verts, location, geometry_ID):
         self.__vertices = verts
-        super().__init__(location, geoID)
+        super().__init__(location, geometry_ID)
 
     def getVertices(self):
         return self.__vertices
@@ -37,9 +38,9 @@ class CompGeom(Geometry):
 class Vertex(Geometry):
     """Single vertex representation"""
 
-    def __init__(self, coords, geoID):
+    def __init__(self, coords, geometry_ID):
         self.__position = coords
-        super().__init__(self.__position, geoID)
+        super().__init__(self.__position, geometry_ID)
 
 ########################################################################################################################
 ########################################################################################################################
@@ -48,17 +49,38 @@ class Vertex(Geometry):
 class Edge(CompGeom):
     """Edge representation, consists of 2 vertices"""
 
-    def __init__(self, vert0, vert1, geoID):
+    def __init__(self, vert0, vert1, geometry_ID):
+        self.__vert0 = vert0
+        self.__vert1 = vert1
         position = [divisor / 2 for divisor in list(map(add, vert1.getLocation(), vert0.getLocation()))]
-        super().__init__([vert0, vert1], position, geoID)
+        super().__init__([self.__vert0, self.__vert1], position, geometry_ID)
+
+    def getLength(self):
+        v0_loc = self.__vert0.getLocation()
+        v1_loc = self.__vert1.getLocation()
+        return sqrt((v0_loc[0] - v1_loc[0])**2 + (v0_loc[1] - v1_loc[1])**2 + (v0_loc[2] - v1_loc[2])**2)
 
 ########################################################################################################################
 ########################################################################################################################
 
 
 class Face(CompGeom):
-    """Quad face representation, consists of 4 vertices"""
+    """Quad face representation, consists of 4 vertices. Edges are calculated internally"""
 
-    def __init__(self, vert0, vert1, vert2, vert3, geoID):
+    def __init__(self, vert0, vert1, vert2, vert3, geometry_ID):
+        self.__vert0 = vert0
+        self.__vert1 = vert1
+        self.__vert2 = vert2
+        self.__vert3 = vert3
+        self.__edge0 = Edge(self.__vert0, self.__vert1, 0)
+        self.__edge1 = Edge(self.__vert1, self.__vert2, 1)
+        self.__edge2 = Edge(self.__vert2, self.__vert3, 2)
+        self.__edge3 = Edge(self.__vert3, self.__vert0, 3)
         position = [divisor/2 for divisor in list(map(add, vert2.getLocation(), vert0.getLocation()))]
-        super().__init__([vert0, vert1, vert2, vert3], position, geoID)
+        super().__init__([self.__vert0, self.__vert1, self.__vert2, self.__vert3], position, geometry_ID)
+
+    def getEdgeLengths(self):
+        return [self.__edge0.getLength(), self.__edge1.getLength()]#
+
+    def getArea(self):
+        return self.__edge0.getLength() * self.__edge1.getLength()
--- main.py
+++ main.py
@@ -3,22 +3,9 @@ from CellGeometry import *
 from Arbiter import Arbiter
 from FileIO import FileIO
 
-print('Hello World')
+print('Hello World, this is the testing stage for the cell structure as of now')
 
 loc = [1, 1, 1]
 dim = [2, 2, 2]
 
 c = Cell(3, loc, dim, {})
-
-
-v1 = Vertex([1, 1, 1], 0)
-v2 = Vertex([2, 2, 2], 1)
-v3 = Vertex([3, 3, 3], 2)
-v4 = Vertex([4, 4, 4], 3)
-e = Edge(v1, v2, 0)
-f = Face(v1, v2, v3, v4, 0)
-
-
-print(e.getLocation())
-print(e.getVertices())
-print(e.getVertexLocations())
