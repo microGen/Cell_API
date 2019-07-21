c8c567fd45f7d2d0e99e1e8c29e9d4d2bb2646b7
nlhw <microGen@users.noreply.github.com>
2019-07-21 12:51:49 +0200

    Container can now search for data fields, load JSON file. Added JSON test file

--- Container.py
+++ Container.py
@@ -1,13 +1,12 @@
 from random import randint
+from math import inf, sqrt
 import json
 
 class Container:
     def __init__(self, filename):
-        self.__grid_data_length = 0
-        self.__grid_data = []
         self.__infile = open(filename, 'r')
         self.__grid = json.load(self.__infile)
-        pass
+        self.__grid_length = len(self.__grid)
 
     ####################################################################################################################
 
@@ -22,23 +21,28 @@ class Container:
         """Calculates the Euclidean distance between 'coordinates' and grid data and returns the data of the closest
         grid point"""
 
-        data_index = randint(0, self.__grid_data_length)
+        data_index = randint(0, self.__grid_length)
         eucl_dist_prev = inf
 
-        for i in range(self.__grid_data_length):
-            eucl_dist = sqrt(sum([(a - b) ** 2 for a, b in zip(coordinates, self.__grid_data[i][1:4])]))
+        for i in range(self.__grid_length):
+            str_i = str(i)
+            eucl_dist = sqrt(sum([(a - b) ** 2 for a, b in zip(coordinates, self.__grid[str_i]["Location"])]))
 
             if (eucl_dist < eucl_dist_prev):
-                data_index = i
+                data_index = str_i
             eucl_dist_prev = min(eucl_dist, eucl_dist_prev)
 
-        return self.__grid_data[data_index]
+        return self.__grid[data_index]
 
     ####################################################################################################################
 
 
     def getEnclosedData(self, minmax_coordinates):
         """Returns a list of grid data of all grid elements within 'minmax_coordinates'"""
+
+        x = 0
+        y = 1
+        z = 2
 
         x_min = minmax_coordinates[0][0]
         x_max = minmax_coordinates[0][1]
@@ -48,10 +52,43 @@ class Container:
         z_max = minmax_coordinates[2][1]
         data_list = []
 
-        for i in range(self.__grid_data_length):
-            if x_min <= self.__grid_data[i][1] <= x_max and \
-                                    y_min <= self.__grid_data[i][2] <= y_max and \
-                                    z_min <= self.__grid_data[i][3] <= z_max:
-                data_list.append(self.__grid_data[i])
+        for i in range(self.__grid_length):
+            str_i = str(i)
+            if x_min <= self.__grid[str_i]['Location'][x] <= x_max and \
+                                    y_min <= self.__grid[str_i]["Location"][y] <= y_max and \
+                                    z_min <= self.__grid[str_i]["Location"][z] <= z_max:
+                data_list.append(self.__grid[str_i])
 
         return data_list
+
+    ####################################################################################################################
+
+
+    def getData(self, minmax_coordinates):
+        """Always returns data. If getEnclosedData() returns empty, getNearestData() returns closest data point"""
+
+        data_list = self.getEnclosedData(minmax_coordinates)
+        if data_list == []:
+            coords_x = (minmax_coordinates[0][0] + minmax_coordinates[0][1]) / 2
+            coords_y = (minmax_coordinates[1][0] + minmax_coordinates[1][1]) / 2
+            coords_z = (minmax_coordinates[2][0] + minmax_coordinates[2][1]) / 2
+            coordinates = [coords_x, coords_y, coords_z]
+            data_list = [self.getNearestData(coordinates)]
+
+        return data_list
+
+    ####################################################################################################################
+
+
+    def dumpData(self):
+        """Returns input data as a string"""
+
+        return json.dumps(self.__grid)
+
+    ####################################################################################################################
+
+
+    def lengthOfData(self):
+        """Returns length of input grid data list"""
+
+        return self.__grid_length
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from Cell import Cell, CellFinal
+from Container import Container
 from CellGeometry import *
 from Arbiter import Arbiter
 from FileIO import FileIO
+import json
+debug_cell = False
+debug_json = True
 
 print('Hello World, this is the testing stage for the cell structure as of now')
 
@@ -9,36 +13,46 @@ loc = [1, 1, 1]
 dim = [2, 2, 2]
 
 c = Cell(3, loc, dim, {})
-print('Cell data:')
-print('ID:\t\t\t', c.ID())
-print('Pos:\t\t', c.location())
-print('Dims:\t\t', c.dimensions())
-print('MinMax:\t\t', c.minmax())
-print('Volume:\t\t', c.volume())
-print('CoreProps:\t', c.coreProperties())
-print('ExtProps:\t', c.extProperties())
+cont = Container("json_test_input.txt")
 
-print(c.vertices(), '\n')
-print(c.vertices(1), '\n')
-print(c.vertices([0, 2, 3]), '\n')
-print(c.edges(), '\n')
-print(c.faces(), '\n')
+if debug_cell:
+    print('Cell data:')
+    print('ID:\t\t\t', c.ID())
+    print('Pos:\t\t', c.location())
+    print('Dims:\t\t', c.dimensions())
+    print('MinMax:\t\t', c.minmax())
+    print('Volume:\t\t', c.volume())
+    print('CoreProps:\t', c.coreProperties())
+    print('ExtProps:\t', c.extProperties())
+    print(c.vertices(), '\n')
+    print(c.vertices(1), '\n')
+    print(c.vertices([0, 2, 3]), '\n')
+    print(c.edges(), '\n')
+    print(c.faces(), '\n')
 
-class test1:
-    def __init__(self, indata):
-        self.__indata = indata
+if debug_json:
+    print('Input Data:')
+    print(cont.dumpData())
+    print(cont.getNearestData([-432432, -42343242, 4234324]))
+    print(cont.getEnclosedData([[4, 5], [4, 5], [4, 5]]))
+    print(cont.getData([[4, 5], [4, 5], [4, 5]]))
+    print('Data fields:\t\t', cont.lengthOfData())
 
-    def murks(self):
-        for i in range(len(self.__indata)):
-            self.__indata[i] + 1
-
-    def hurks(self, indata2):
-        for i in range(len(indata2)):
-            indata2[i]+1
-
-a = [1, 2, 3]
-t = test1(a)
-t.murks()
-print(a)
-t.hurks(a)
-print(a)
+# class test1:
+#     def __init__(self, indata):
+#         self.__indata = indata
+#
+#     def murks(self):
+#         for i in range(len(self.__indata)):
+#             self.__indata[i] + 1
+#
+#     def hurks(self, indata2):
+#         for i in range(len(indata2)):
+#             indata2[i]+1
+#
+# a = [1, 2, 3]
+# t = test1(a)
+# t.murks()
+# print(a)
+# t.hurks(a)
+# print(a)
